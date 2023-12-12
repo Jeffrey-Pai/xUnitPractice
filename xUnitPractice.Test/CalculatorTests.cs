b1c4c7a12df164110cb365ab8a478219910716a5
Jeffrey-Pai <s940112jeffrey@gmail.com>
2023-12-13 00:00:11 +0800

    init xuint calculator case

---
+++ xUnitPractice.Test/CalculatorTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace xUnitPractice.Test
+{
+    public class CalculatorTests
+    {
+        //告訴編譯器要執行的測試方法
+        [Fact]
+        public void Correct1()
+        {
+            // Arrange
+            double expected = 20;
+
+            // Act
+            var actual = Calculator.Add(5, 15);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Correct2()
+        {
+            // Arrange
+            double expected = 20;
+
+            // Act
+            var actual = Calculator.Add(7, 13);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Correct3()
+        {
+            // Arrange
+            double expected = 10;
+
+            // Act
+            var actual = Calculator.Add(15, -5);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Correct4()
+        {
+            // Arrange
+            double expected = 0;
+
+            // Act
+            var actual = Calculator.Add(-5, 5);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Correct5()
+        {
+            // Arrange
+            double expected = -10;
+
+            // Act
+            var actual = Calculator.Add(-15, 5);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ xUnitPractice/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace xUnitPractice
+{
+    public static class Calculator
+    {
+        public static double Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
